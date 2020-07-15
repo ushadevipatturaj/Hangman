4a2fd0a565be6810cc2415a781d4e8952db7972d
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-15 19:45:07 +0200

    Hangman

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,11 +1,38 @@
 import random
 # Write your code here
+
 list_choice = ('python', 'java', 'kotlin', 'javascript')
+print('H A N G M A N')
 computer_choice = random.choice(list_choice)
-guessword = str(computer_choice[:3]).ljust(len(computer_choice), '-')
-print('H A N G M A N\nGuess the word {}:'.format(guessword))
-user_choice = input()
-if user_choice == computer_choice:
-    print('You survived!')
+guessword = '-' * len(computer_choice)
+temp = set()
+fail_case = set()
+fail_counter = 1
+
+while fail_counter <= 8 and guessword != computer_choice:
+    print('\n{}'.format(guessword))
+    user_choice = input("Input a letter: ")
+    guessword_temp = ''
+    if (user_choice in fail_case) or (user_choice not in fail_case and user_choice in temp):
+        print('No improvements')
+        fail_counter += 1
+        fail_case.add(user_choice)
+        continue
+    elif user_choice not in computer_choice:
+        print('No such letter in the word')
+        fail_counter += 1
+        fail_case.add(user_choice)
+        continue
+    elif user_choice in computer_choice:
+        temp.add(user_choice)
+        for char in computer_choice:
+            if char in temp:
+                guessword_temp += char
+                continue
+            guessword_temp += '-'
+        guessword = guessword_temp
+
+if guessword == computer_choice:
+    print("\n{}\nYou guessed the word!\nYou survived!".format(guessword))
 else:
-    print('You are hanged!')
+    print("You are hanged!")

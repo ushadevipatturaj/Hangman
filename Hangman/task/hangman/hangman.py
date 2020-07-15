dea99397b402c8419ec4836fc73ebd86ea1c59cd
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-15 14:49:50 +0200

    Mystery set

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,8 +1,10 @@
 import random
 # Write your code here
-user_choice = input()
 list_choice = ('python', 'java', 'kotlin', 'javascript')
 computer_choice = random.choice(list_choice)
+guessword = str(computer_choice[:3]).ljust(len(computer_choice), '-')
+print('H A N G M A N\nGuess the word {}:'.format(guessword))
+user_choice = input()
 if user_choice == computer_choice:
     print('You survived!')
 else:
---
+++ Problems/Mystery set/task.py
@@ -0,0 +1,5 @@
+# mystery_set has been defined
+string = input()
+# delete string from mystery_set
+if string in mystery_set:
+    mystery_set.remove(string)
